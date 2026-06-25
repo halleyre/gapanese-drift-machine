5d609ee11ce026baed7e399679b7b0ff2b065be6
Yoshi <james.wilson.fnm@gmail.com>
2026-06-25 14:54:53 +1000

    Fixed typo

--- godot/Scripts/player.gd
+++ godot/Scripts/player.gd
@@ -37,7 +37,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		y_direction = -1
 		
-	if Input.is_action_just_released("uia_down") and y_direction == 1:
+	if Input.is_action_just_released("ui_down") and y_direction == 1:
 		if Input.is_action_pressed("ui_up"):
 			y_direction = -1
 		else:
